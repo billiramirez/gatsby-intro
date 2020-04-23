b70666cf217cd1554adfe60d423640151becd5e8
Billi Ramirez <ramirezbilly42@outlook.com>
2020-04-23 15:09:27 -0600

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from 'react';
+import Layout from '../components/layout';
+import Hero from '../components/hero';
+import Insta from '../components/insta';
+import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/postPreview';
 
-export default () => <div>Hello Frontend Masters!</div>;
+export default () => {
+  const posts = usePosts();
+  return (
+    <>
+      <Hero />
+      <Layout>
+        <h2>Read my Blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post} />
+        ))}
+        <Insta />
+      </Layout>
+    </>
+  );
+};
